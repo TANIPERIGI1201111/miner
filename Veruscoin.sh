28ac9e57c40d4d1868e35556a79a66beb2645367
TANIPERIGI1201111 <75982339+TANIPERIGI1201111@users.noreply.github.com>
2021-06-28 20:38:09 +0700

    Veruscoin.sh
    Testrepo

---
+++ Veruscoin.sh
@@ -0,0 +1,5 @@
+# Veruscoin
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RQFxDgPf9SKswWkSi31hnGcU1W9DT4nEr4.newbiepolka -p x --cpu 4
